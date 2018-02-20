e2af437f0ea01d20fded5a3e3273b8252628e2f2
Umara Amir <amirumara777@gmail.com>
2018-02-20 10:30:57 +0700

    Adding Db

---
+++ RaceApp/RaceApp/RaceAppDb/RaceDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceApp.RaceAppDb
+{
+    public interface RaceDataStore<T>
+    {
+        Task<bool> InsertData(T item);
+        Task<bool> DeleteData(string PK);
+        Task<bool> UpdateData(string PK, T item);
+        Task<T> GetItem(string PK);
+        Task<IEnumerable<T>> GetItems();
+    }
+}
